e83e0d11f52a7543e82d937d84441f2151e88cea
mh226eh <mh226eh@student.lnu.se>
2025-09-16 01:22:50 +0200

    Added a simple about on home page + favicon

--- src/components/Hero/Hero2.tsx
+++ src/components/Hero/Hero2.tsx
@@ -4,11 +4,30 @@ const Hero2 = () => (
   <section className="hero-section container py-5">
     <div className="container">
       <div className="row align-items-center">
-        <div className="col-md-7">
-          <h2>LET ME INTRODUCE MYSELF</h2>
-          <p>
-            I have always been a bit of a nerd, with a passion for technology and a love for learning. My journey in the tech world has been fueled by curiosity and a desire to create.
+        <div className="col-md-7 home-about-description">
+          <h1 style={{ fontSize: "2.6em" }}>
+            LET ME <span className="purple">INTRODUCE</span> MYSELF
+          </h1>
+
+          <p className="home-about-body">
+            I have always been a bit of a nerd, with a passion for technology and a love for learning. 
+            My journey in the tech world is just getting started! But, I think I have learned a thing or two… 🤓
           </p>
+
+          <p className="home-about-body">
+            I am proficient in classics like
+            <i>
+              <b className="purple"> Java, Python, C#, and JavaScript </b>
+            </i>
+            <br />
+            <br />
+            Currently, I’m expanding my skill set by diving into
+            <i>
+              <b className="purple"> TypeScript, React, and most importantly </b>
+            </i>
+          </p>
+          
+          <h2 className="data-science">DATA SCIENCE</h2>
         </div>
         <div className="col-md-5 text-center">
           <img
